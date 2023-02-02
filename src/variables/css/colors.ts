d7a96189b0f33d41915f574075761cd5cdf03510
sohodoll <kovalevvtl@gmail.com>
2023-02-02 22:25:46 +0300

    refactor: add camelCase to color types

--- src/variables/css/colors.ts
+++ src/variables/css/colors.ts
@@ -1,12 +1,12 @@
-export type ColorKeys = 'red' | 'green' | 'blue' | 'redhover' | 'greenhover' | 'bluehover' | 'lightgray' | 'darkgray'
+export type ColorKeys = 'red' | 'green' | 'blue' | 'redHover' | 'greenHover' | 'blueHover' | 'lightGray' | 'darkGray'
 
 export const colors: Record<ColorKeys, string> = {
   red: 'red-500',
   green: 'green-500',
   blue: 'blue-500',
-  redhover: 'red-700',
-  greenhover: 'green-700',
-  bluehover: 'blue-700',
-  lightgray: 'gray-100',
-  darkgray: 'gray-800',
+  redHover: 'red-700',
+  greenHover: 'green-700',
+  blueHover: 'blue-700',
+  lightGray: 'gray-100',
+  darkGray: 'gray-800',
 }
