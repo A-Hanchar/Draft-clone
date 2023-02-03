5848b0b124e7f7d6d6c814eafb119927abc69b54
Artsiom Hanchar <artsiom.hanchar@gmail.com>
2023-02-03 13:45:10 +0400

    feat: add common components for not found page

--- src/components/Layout/Layout.ts
+++ src/components/Layout/Layout.ts
@@ -6,9 +6,15 @@ import { type PropsWithChildren } from 'types'
 
 export const Layout = ({ children }: PropsWithChildren) => {
   const fragment = document.createDocumentFragment()
-  const main = createElementWithClassNameAndAppendNode({ tagName: 'main', children })
+  const main = createElementWithClassNameAndAppendNode({
+    tagName: 'main',
+    children,
+  })
 
   addClassnameToElement({ element: Body, classname: 'bg-gray-100' })
+  addClassnameToElement({ element: Body, classname: 'grid' })
+  addClassnameToElement({ element: Body, classname: 'grid-rows-[auto_1fr_auto]' })
+  addClassnameToElement({ element: Body, classname: 'min-h-screen' })
 
   fragment.append(Header(), main, Footer())
 
--- src/pages/NotFound/NotFound.ts
+++ src/pages/NotFound/NotFound.ts
@@ -1,3 +1,16 @@
+import { Link } from 'components/Link'
 import { createElementWithClassNameAndAppendNode } from 'helpers'
+import { routerPathes } from 'router'
 
-export const NotFound = () => createElementWithClassNameAndAppendNode({ tagName: 'div', children: 'Not Found Page' })
+export const NotFound = () => {
+  const title = createElementWithClassNameAndAppendNode({ tagName: 'h1', children: '404' })
+  const additionalText = createElementWithClassNameAndAppendNode({ tagName: 'h2', children: 'Page Not Fount' })
+
+  const wrapper = createElementWithClassNameAndAppendNode({
+    tagName: 'div',
+    children: [title, additionalText, Link({ href: routerPathes.home, children: 'Go Home' })],
+    classname: ['flex', 'flex-col', 'justify-center', 'items-center', 'h-full'],
+  })
+
+  return wrapper
+}
