c78a6b9d9664c368511ae5d09c453d71b2166ba5
yauheniZabotsin <yauheni.zabotsin@gmail.com>
2023-02-03 15:54:58 +0300

    feat: revome excess arguments

--- src/components/Button/types.ts
+++ src/components/Button/types.ts
@@ -3,7 +3,6 @@ import { type PropsWithChildren } from 'types'
 export type ButtonProps = PropsWithChildren<{
   classname?: string
   color: 'primary' | 'alert' | 'success'
-  textcontent?: string
   onclick?: () => void
   type?: string
   disabled?: boolean
