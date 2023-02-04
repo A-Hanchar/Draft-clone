1387f8da75a976d52573d80a4292a39d6e6d4ce2
yauheniZabotsin <yauheni.zabotsin@gmail.com>
2023-02-05 00:27:04 +0300

    refactor: delite return in GitHubLink

--- src/components/GitHubLink/GitHubLink.ts
+++ src/components/GitHubLink/GitHubLink.ts
@@ -2,12 +2,10 @@ import { Link } from 'components/Link'
 
 import { type GitHubLinkProps } from './types'
 
-export const GitHubLink = ({ username, name }: GitHubLinkProps) => {
-  const a = Link({
+export const GitHubLink = ({ username, name }: GitHubLinkProps) =>
+  Link({
     href: `https://github.com/${username}`,
     target: '_blank',
     classname: 'block',
     children: name,
   })
-  return a
-}
