c90021ff46cc4c994935e47c940e4a93f477ee2e
Artsiom Hanchar <artsiom.hanchar@gmail.com>
2023-02-05 14:57:27 +0400

    feat: remove modal from not found page

--- src/pages/NotFound/NotFound.ts
+++ src/pages/NotFound/NotFound.ts
@@ -1,25 +1,10 @@
-import { Body } from 'components/Body'
-import { Button } from 'components/Button'
 import { Link } from 'components/Link'
-import { Modal } from 'components/Modal'
 import { Text } from 'components/Text'
 import { createElementWithClassNameAndAppendNode } from 'helpers'
 import { routerPathes } from 'router'
 
-export const NotFound = () => {
-  const modalContent = createElementWithClassNameAndAppendNode({
-    tagName: 'div',
-  })
-
-  const modal = Modal({ children: modalContent, title: 'Congrats' })
-
-  const openModal = () => {
-    Body.append(modal)
-  }
-
-  const button = Button({ children: 'Open Modal', onclick: openModal })
-
-  return createElementWithClassNameAndAppendNode({
+export const NotFound = () =>
+  createElementWithClassNameAndAppendNode({
     tagName: 'div',
     children: [
       Text({ tagName: 'h1', innerText: 'Error: 404', classname: 'text-9xl m-0', weight: 900 }),
@@ -32,8 +17,6 @@ export const NotFound = () => {
         classname: 'text-xl min-w-[15.625rem] flex justify-center',
         weight: 600,
       }),
-      button,
     ],
     classname: 'flex flex-col justify-center items-center h-full gap-6',
   })
-}
