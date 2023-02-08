4baafe0675506818d8816a017afe7a95f9673aee
Artsiom Hanchar <artsiom.hanchar@gmail.com>
2023-02-08 22:37:26 +0400

    fix: add min-h-screen class for not found page wrapper

--- src/pages/NotFound/NotFound.ts
+++ src/pages/NotFound/NotFound.ts
@@ -18,5 +18,5 @@ export const NotFound = () =>
         weight: 600,
       }),
     ],
-    classname: 'flex flex-col justify-center items-center h-full gap-6',
+    classname: 'flex flex-col justify-center items-center min-h-screen gap-6',
   })
