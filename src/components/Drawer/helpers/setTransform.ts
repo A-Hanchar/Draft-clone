396cf588b42fbf82a06b4551de037d5004d811df
yauheniZabotsin <yauheni.zabotsin@gmail.com>
2023-02-15 16:51:00 +0300

    feat: del setTransform function

--- src/components/Drawer/helpers/setTransform.ts
+++
@@ -1,4 +0,0 @@
-export const setTransform = (progress: number, distance: number, element: HTMLDivElement, isDirection: boolean) => {
-  const translate = isDirection ? progress * distance - distance - 50 : -progress * distance - 50
-  element.style.transform = `translateX(${translate}px)`
-}
