9897157ae0f31c3100b548da47133a2c561b9e83
sohodoll <kovalevvtl@gmail.com>
2023-02-17 22:00:56 +0300

    refactor: refactor update document

--- src/api/documents/updateDocument.ts
+++ src/api/documents/updateDocument.ts
@@ -1,16 +1,13 @@
 import { auth, getUserDocumentRef } from 'api'
-import { onAuthStateChanged } from 'firebase/auth'
 import { uploadBytes } from 'firebase/storage'
 import { createNewTextFile } from 'helpers'
 
-export const updateDocument = (documentId: string, content: string) => {
+export const updateDocument = async (documentId: string, content: string) => {
   const file = createNewTextFile(content)
 
-  onAuthStateChanged(auth, async () => {
-    if (auth.currentUser) {
-      if (documentId) {
-        await uploadBytes(getUserDocumentRef(documentId), file)
-      }
+  if (auth.currentUser) {
+    if (documentId) {
+      await uploadBytes(getUserDocumentRef(documentId), file)
     }
-  })
+  }
 }
