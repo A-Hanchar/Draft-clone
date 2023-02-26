f88d1df63b660b2797abbf79b6b2503d235f8f28
Artsiom Hanchar <artsiom.hanchar@gmail.com>
2023-02-26 19:05:00 +0400

    feat: add template of changing color mode

--- src/components/Header/Header.ts
+++ src/components/Header/Header.ts
@@ -3,11 +3,22 @@ import { type PropsWithClassname } from 'types'
 
 import { Logo } from './Logo'
 
+import { ToggleColorMode } from './ToggleColorMode'
+
 import { ToggleLanguage } from './ToggleLanguage'
 
-export const Header = ({ classname }: PropsWithClassname) =>
-  createElementWithClassNameAndAppendNode({
+export const Header = ({ classname }: PropsWithClassname) => {
+  const appHandlerWrapper = createElementWithClassNameAndAppendNode({
+    tagName: 'div',
+    children: [ToggleColorMode(), ToggleLanguage()],
+    classname: 'flex gap-4',
+  })
+
+  const header = createElementWithClassNameAndAppendNode({
     tagName: 'header',
-    children: [Logo(), ToggleLanguage()],
+    children: [Logo(), appHandlerWrapper],
     classname: getTruthyClasses(['flex justify-between px-10 shadow-2xl', classname]),
   })
+
+  return header
+}
--- src/instances/localStorageInstanse.ts
+++ src/instances/localStorageInstanse.ts
@@ -1,5 +1,6 @@
 /* eslint-disable class-methods-use-this */
 import { LANGUAGE } from 'i18n'
+import { THEME_COLOR_MODE } from 'variables/css'
 
 import { LOCAL_STORAGE_KEY } from './types'
 
@@ -24,6 +25,14 @@ class LocaltorageInstanse {
 
   getLanguage() {
     return this.getValue<LANGUAGE>(LOCAL_STORAGE_KEY.LANGUAGE) ?? LANGUAGE.EN
+  }
+
+  setThemeColorMode(colorMode: THEME_COLOR_MODE) {
+    localStorage.setItem(LOCAL_STORAGE_KEY.THEME_COLOR_MODE, colorMode)
+  }
+
+  getThemeColorMode() {
+    return this.getValue<THEME_COLOR_MODE>(LOCAL_STORAGE_KEY.THEME_COLOR_MODE) ?? THEME_COLOR_MODE.LIGHT
   }
 }
 
